b7963c5d7c57bf0d102f5a14aa4de22f2925bdfa
Artem Bobyr <artembobir@gmail.com>
2020-08-19 20:21:14 +0300

    update repo

--- CRMPerfUIFramework/runTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ITER=0
-
-while [ "$ITER" -le "15" ]
-do
-  echo "Looping ... number $ITER"
-  mvn clean test -Dbrowser=chrome -Dtest=PharmaDataMigration -Dgroups=user08
-  ITER=$((ITER + 1 ))
-done
